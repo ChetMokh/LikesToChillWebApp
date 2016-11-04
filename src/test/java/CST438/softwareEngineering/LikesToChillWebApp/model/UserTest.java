871620e5b95252a4440a1868577c1e48da3949a5
ChetMokh <aaria@gmail.com>
2016-11-03 19:45:31 -0700

    added test class for user

---
+++ src/test/java/CST438/softwareEngineering/LikesToChillWebApp/model/UserTest.java
@@ -0,0 +1,14 @@
+package CST438.softwareEngineering.LikesToChillWebApp.model;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class UserTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
