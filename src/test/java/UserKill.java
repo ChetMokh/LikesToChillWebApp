3f1f14cbb703ff786593d1c759bf43f26326fa81
BRX0102 <brono@csumb.edu>
2016-11-07 23:02:31 -0800

    Added Killing John
    Summary: Make John. Kill John.
    
    Test Plan: John will die.
    
    Reviewers: chetmokh
    
    Subscribers:

--- src/test/java/UserKill.java
+++ src/test/java/UserKill.java
@@ -12,6 +12,8 @@ import org.junit.Before;
 import org.junit.Test;
 
 import CST438.softwareEngineering.LikesToChillWebApp.model.User;
+
+//Brian Test
 public class UserKill {
 	private Connection connection;
 	
