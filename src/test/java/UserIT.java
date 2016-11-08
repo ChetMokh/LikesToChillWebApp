122ee666ae08e7af2a43f0df536cb8e3e809a9c0
ChetMokh <aaria@gmail.com>
2016-11-07 23:52:36 -0800

    added integration test
    Summary: 	integration test
    
    Test Plan: 	tesing get messages
    
    Reviewers: brono
    
    Differential Revision: http://csumbse.net/D252

--- src/test/java/UserIT.java
+++ src/test/java/UserIT.java
@@ -1,40 +1,132 @@
-import static org.junit.Assert.*;
+import java.io.IOException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.Scanner;
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-import java.util.ArrayList;
-import java.util.List;
+public class UserIT{
 
-
-import org.junit.Before;
-import org.junit.Test;
-
-import CST438.softwareEngineering.LikesToChillWebApp.model.User;
-public class UserIT {
-	private Connection connection;
-	
-	@Before
-	public void getConnection() throws ClassNotFoundException, SQLException, InstantiationException, IllegalAccessException{
-		Class.forName("com.mysql.jdbc.Driver").newInstance();
+	@Test
+	public void apiTesting1() throws Exception {
 		
-		connection = DriverManager.getConnection(
-				"jdbc:mysql://likestochill.c2xl5ywnufyt.us-west-1.rds.amazonaws.com:3306/LikesToChill", "ChillAdmin",
-				"Chill438");
+		try {
+				URL url = new URL("http://likestochill.heidyzqyc2.us-west-2.elasticbeanstalk.com/webapi/users");
+				HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+				conn.setRequestMethod("GET");
+				conn.setRequestProperty("Accept", "application/json");
+	
+				if (conn.getResponseCode() != 200) {
+					throw new RuntimeException(" HTTP error code : "+ conn.getResponseCode());
+				}
+	
+				Scanner scan = new Scanner(url.openStream());
+				String entireResponse = new String();
+				while (scan.hasNext())
+					entireResponse += scan.nextLine();
+	
+				System.out.println("Response : "+entireResponse);
+	
+				scan.close();
+	
+				JSONObject obj = new JSONObject(entireResponse );
+				String responseCode = obj.getString("status");
+				System.out.println("status : " + responseCode);
+	
+				JSONArray arr = obj.getJSONArray("results");
+				
+				String[] expectedResult = {"Sean","Arash","Brian","Peter","Ben"};
+				
+				for (int i = 0; i < 4; i++) {
+					String firstName = arr.getJSONObject(i).getString("fName");
+					System.out.println("fName : " + firstName);
+					String secondName = arr.getJSONObject(i).getString("lName");
+					System.out.println("lName : " + secondName);
+	
+					//validating Address as per the requirement
+					if(firstName.equalsIgnoreCase(expectedResult[i]))
+					{
+						System.out.println("Address is as Expected");
+					}
+					else
+					{
+						System.out.println("Address is not as Expected");
+					}
+				}
+	
+				conn.disconnect();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+	
+		} catch (IOException e) {
+	
+			e.printStackTrace();
+	
+		}
+	
 	}
+
 	
 	@Test
-	public void readDataFromDB() throws SQLException, ClassNotFoundException{
-
-		String selectTableSQL = "SELECT * FROM Users WHERE UserId = 1";
-		java.sql.Statement statement = connection.createStatement();
-		ResultSet rs = statement.executeQuery(selectTableSQL);
-
-		rs.next();
-		String firstName = rs.getString("FirstName");
+	public void apiTesting2() throws Exception {
 	
-		assertEquals(firstName, "Sean");
-		//hello test
+		try {
+				URL url = new URL("view-source:http://likestochill.heidyzqyc2.us-west-2.elasticbeanstalk.com/webapi/users/1");
+				HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+				conn.setRequestMethod("GET");
+				conn.setRequestProperty("Accept", "application/json");
+	
+				if (conn.getResponseCode() != 200) {
+					throw new RuntimeException(" HTTP error code : "+ conn.getResponseCode());
+				}
+	
+				Scanner scan = new Scanner(url.openStream());
+				String entireResponse = new String();
+				while (scan.hasNext())
+					entireResponse += scan.nextLine();
+	
+				System.out.println("Response : "+entireResponse);
+	
+				scan.close();
+	
+				JSONObject obj = new JSONObject(entireResponse );
+				String responseCode = obj.getString("status");
+				System.out.println("status : " + responseCode);
+	
+				JSONArray arr = obj.getJSONArray("results");
+				
+				
+				
+				
+				String firstName = arr.getJSONObject(0).getString("fName");
+				System.out.println("fName : " + firstName);
+				
+				String secondName = arr.getJSONObject(0).getString("lName");
+				System.out.println("lName : " + secondName);
+
+				//validating Address as per the requirement
+				if(firstName.equalsIgnoreCase("Sean"))
+				{
+					System.out.println("Address is as Expected");
+				}
+				else
+				{
+					System.out.println("Address is not as Expected");
+				}
+				
+	
+				conn.disconnect();
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+	
+		} catch (IOException e) {
+	
+			e.printStackTrace();
+	
+		}
+	
 	}
 }
