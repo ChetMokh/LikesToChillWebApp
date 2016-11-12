26f273e177ce2a19b5b17cffdbbc0b44a8330f29
ChetMokh <aaria@gmail.com>
2016-11-11 18:51:42 -0800

    movie class
    added get method for movie class and edited the movie class based on
    the new design.

---
+++ src/main/java/CST438/softwareEngineering/LikesToChillWebApp/resources/MovieResources.java
@@ -0,0 +1,47 @@
+package CST438.softwareEngineering.LikesToChillWebApp.resources;
+
+import java.sql.SQLException;
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import CST438.softwareEngineering.LikesToChillWebApp.model.Movie;
+import CST438.softwareEngineering.LikesToChillWebApp.model.User;
+import CST438.softwareEngineering.LikesToChillWebApp.service.MovieDAO;
+import CST438.softwareEngineering.LikesToChillWebApp.service.UserDAO;
+
+@Path ("movies")
+public class MovieResources {
+
+	
+	MovieDAO userMovies = new MovieDAO();
+	
+	
+//	@GET
+//	@Produces(MediaType.APPLICATION_JSON)
+//    public List<User> getAllUsers() throws ClassNotFoundException, SQLException, InstantiationException, IllegalAccessException {
+//        users.getConnection();
+//		return users.findAll();
+//    }
+	
+	@GET
+	@Path("/{userId}")
+	@Produces(MediaType.APPLICATION_JSON)
+    public List<Movie> getUserById(@PathParam("userId") int userId ) throws ClassNotFoundException, SQLException, InstantiationException, IllegalAccessException {
+        userMovies.getConnection();
+		return userMovies.findMoviesByUserId(userId);
+    }
+	
+//	@POST
+//	@Path("/post")
+//	@Produces(MediaType.APPLICATION_JSON)
+//	public String getPost() {
+//		return "POST works";
+//	}
+	
+}
