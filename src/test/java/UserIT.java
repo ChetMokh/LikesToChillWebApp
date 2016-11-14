1c26e55afabe958ab61102a107a4268353cd19cb
ChetMokh <aaria@gmail.com>
2016-11-14 00:21:56 -0800

    added another integration test

--- src/test/java/UserIT.java
+++ src/test/java/UserIT.java
@@ -16,74 +16,9 @@ import org.testng.annotations.Test;
 import bsh.ParseException;
 
 public class UserIT{
-
-//	@Test
-//	public void apiTesting1() throws Exception {
-//		
-//		try {
-//
-//				URL url = new URL("http://likestochill.heidyzqyc2.us-west-2.elasticbeanstalk.com/webapi/users");
-//				HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-//				conn.setRequestMethod("GET");
-//				conn.setRequestProperty("Accept", "application/json");
-//	
-//				if (conn.getResponseCode() != 200) {
-//					throw new RuntimeException(" HTTP error code : "+ conn.getResponseCode());
-//				}
-//	
-//				Scanner scan = new Scanner(url.openStream());
-//				String entireResponse = new String();
-//				while (scan.hasNext()) {
-//					entireResponse += scan.nextLine();
-//				}
-//				System.out.println("Response : "+ entireResponse);
-//	
-//				scan.close();
-//	
-//
-//				
-////				JSONObject obj = new JSONObject(entireResponse);
-//				
-//				JSONArray obj1 = new JSONArray(entireResponse);
-//
-////				String responseCode = obj.getString(0);
-////				System.out.println("status : " + responseCode);
-//	
-////				JSONArray arr = obj1.getJSONArray("results");
-//				
-//				String[] expectedResult = {"John","Sean","Arash","Brian","Peter","Ben"};
-//				
-//				for (int i = 0; i < 5; i++) {
-//					String firstName = arr.getJSONObject(i).getString("fName");
-//					System.out.println("fName : " + firstName);
-//					String secondName = arr.getJSONObject(i).getString("lName");
-//					System.out.println("lName : " + secondName);
-//	
-//					//validating Address as per the requirement
-//					if(firstName.equalsIgnoreCase(expectedResult[i]))
-//					{
-//						System.out.println("Address is as Expected");
-//					}
-//					else
-//					{
-//						System.out.println("Address is not as Expected");
-//					}
-//				}
-//	
-//				conn.disconnect();
-//		} catch (MalformedURLException e) {
-//			e.printStackTrace();
-//	
-//		} catch (IOException e) {
-//	
-//			e.printStackTrace();
-//	
-//		}
-//	
-//	}
 	
 	@Test
-	public void apiTestIT() throws Exception {
+	public void apiTestIT1() throws Exception {
 		
 		JSONParser parser = new JSONParser();
 
@@ -133,65 +68,57 @@ public class UserIT{
         }   
 		
 	}
+	
+	@Test
+	public void apiTestIT2() throws Exception {
+		
+		JSONParser parser = new JSONParser();
 
-//	
-//	@Test
-//	public void apiTesting2() throws Exception {
-//	
-//		try {
-//				URL url = new URL("view-source:http://likestochill.heidyzqyc2.us-west-2.elasticbeanstalk.com/webapi/users/1");
-//				HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-//				conn.setRequestMethod("GET");
-//				conn.setRequestProperty("Accept", "application/json");
-//	
-//				if (conn.getResponseCode() != 200) {
-//					throw new RuntimeException(" HTTP error code : "+ conn.getResponseCode());
-//				}
-//	
-//				Scanner scan = new Scanner(url.openStream());
-//				String entireResponse = new String();
-//				while (scan.hasNext())
-//					entireResponse += scan.nextLine();
-//	
-//				System.out.println("Response : "+entireResponse);
-//	
-//				scan.close();
-//	
-//				JSONObject obj = new JSONObject(entireResponse );
-//				String responseCode = obj.getString("status");
-//				System.out.println("status : " + responseCode);
-//	
-//				JSONArray arr = obj.getJSONArray("results");
-//				
-//				
-//				
-//				
-//				String firstName = arr.getJSONObject(0).getString("fName");
-//				System.out.println("fName : " + firstName);
-//				
-//				String secondName = arr.getJSONObject(0).getString("lName");
-//				System.out.println("lName : " + secondName);
-//
-//				//validating Address as per the requirement
-//				if(firstName.equalsIgnoreCase("John"))
-//				{
-//					System.out.println("Address is as Expected");
-//				}
-//				else
-//				{
-//					System.out.println("Address is not as Expected");
-//				}
-//				
-//	
-//				conn.disconnect();
-//		} catch (MalformedURLException e) {
-//			e.printStackTrace();
-//	
-//		} catch (IOException e) {
-//	
-//			e.printStackTrace();
-//	
-//		}
-//	
-//	}
+        try {         
+            URL oracle = new URL("http://likestochill.heidyzqyc2.us-west-2.elasticbeanstalk.com/webapi/users/2"); // URL to Parse
+            URLConnection yc = oracle.openConnection();
+            BufferedReader in = new BufferedReader(new InputStreamReader(yc.getInputStream()));
+            
+            String inputLine;
+            while ((inputLine = in.readLine()) != null) { 
+            	
+                JSONArray array = (JSONArray) parser.parse(inputLine);
+                
+                String[] expectedResult = {"Arash"};
+                
+                // Loop through each item
+                int i = 0;
+                for (Object obj : array) {
+
+                    JSONObject result = (JSONObject) obj;
+
+                    String fName = (String) result.get("fName");
+                    System.out.println("f NAME : " + fName);
+                    
+                    String lName = (String) result.get("lName");
+                    System.out.println("l NAME : " + lName);
+
+                    System.out.println("\n");
+                    
+                    if(fName.equalsIgnoreCase(expectedResult[i]))
+    					{
+    						System.out.println("Name is as Expected");
+    					}
+    					else
+    					{
+    						System.out.println("Name is not as Expected");
+    					}
+                    i++;
+                    
+                }
+            }
+            in.close();
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }   
+		
+	}
+
 }
