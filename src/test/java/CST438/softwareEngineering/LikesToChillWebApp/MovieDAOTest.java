00d57a2467d8089612f1d6de1efff49c15cef2a3
ChetMokh <aaria@gmail.com>
2016-12-04 12:41:46 -0800

    added movie DAO test
    Summary: Testing movie add, find, delete and delete all
    
    Test Plan:
    Adding new user, then adding new movie, find it, and delete it
    Then add three movies and delete all movies for the same user
    Reviewers:
    Brono, smofallon
    Subscribers:

---
+++ src/test/java/CST438/softwareEngineering/LikesToChillWebApp/MovieDAOTest.java
@@ -0,0 +1,57 @@
+package CST438.softwareEngineering.LikesToChillWebApp;
+
+import static org.junit.Assert.*;
+
+import java.sql.SQLException;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import CST438.softwareEngineering.LikesToChillWebApp.model.Movie;
+import CST438.softwareEngineering.LikesToChillWebApp.model.User;
+import CST438.softwareEngineering.LikesToChillWebApp.service.MovieDAO;
+import CST438.softwareEngineering.LikesToChillWebApp.service.UserDAO;
+
+public class MovieDAOTest {
+
+	UserDAO test = new UserDAO();
+	MovieDAO movieList = new MovieDAO();
+	int size;
+	int rnd;
+	
+	@Before
+	public void addUser() throws ClassNotFoundException, InstantiationException, IllegalAccessException, SQLException{
+		
+		rnd = 1000 + (int)(Math.random() * 99999999);
+		test.getConnectionToAws();
+		test.addNewUser(
+				new User(String.valueOf(rnd),"dsad", "Aria", "95148", "a@b.com", "M", "10-10-2000", "I am here to chill", "10-10-2200"));
+		movieList.getConnection();
+		
+	}
+	
+	@Test
+	public void addFindDeleteMovieTest() throws SQLException{
+		
+		assertTrue(movieList.addToLikeBucket(new Movie("test", String.valueOf(rnd))));
+		assertEquals("test", movieList.findAllUserLikes(String.valueOf(rnd)).get(0).getMovieTitle());
+		assertTrue(movieList.deleteFromLikeBucket("test", String.valueOf(rnd)));
+		
+	}
+	
+	@Test
+	public void testAddAddDeleteAll() throws SQLException, ClassNotFoundException, InstantiationException, IllegalAccessException{
+	
+		assertTrue(movieList.addToLikeBucket(new Movie("test1", String.valueOf(rnd))));
+		assertTrue(movieList.addToLikeBucket(new Movie("test2", String.valueOf(rnd))));
+		assertTrue(movieList.addToLikeBucket(new Movie("test3", String.valueOf(rnd))));
+		assertEquals("test1", movieList.findAllUserLikes(String.valueOf(rnd)).get(0).getMovieTitle());
+		assertEquals("test2", movieList.findAllUserLikes(String.valueOf(rnd)).get(1).getMovieTitle());
+		assertEquals("test3", movieList.findAllUserLikes(String.valueOf(rnd)).get(2).getMovieTitle());
+		assertTrue(movieList.deleteAllLikeBucket(String.valueOf(rnd)));
+		assertEquals(0, movieList.findAllUserLikes(String.valueOf(rnd)).size());
+	
+	}
+
+	
+}
